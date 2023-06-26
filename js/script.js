f9f37178e8479c3f8d24596f84789e218c75e841
kimbeomjun <gimb52043@gmail.com>
2023-06-26 13:51:13 +0900

    팝업창

--- js/script.js
+++ js/script.js
@@ -1,61 +1,54 @@
-$(document).ready(function(){
-
-    const todayDelete = $(".todayDelete");
-    const justDelete = $(".justDelete");
-    const overlay = $(".overlay");
-    const popUp = $(".popUp");
-    
-    justDelete.click(function(){
-        popUp.hide();
-        overlay.hide();
-    });
-    
-    // 오늘 날짜를 구함
-    const expires = new Date();
-
-    // 쿠키의 만료일을 오늘의 마지막 시간을 ㅗ설정
-    expires.setHours(23,59,59,0);
-
-    // expires 값을 UTC 형식으로 변환 (UTC 문자열은 쿠키의 만료일로 설정가능)
-    const expiresUTC = expires.toUTCString();
-
-    todayDelete.click(function(){
-        // 'popupClosed'라는 이름의 쿠키 생성, 값 true로 설정
-        document.cookie = 'popupClosed=true; expires=' + expiresUTC + '; path=/';
-
-        popUp.hide();
-        overlay.hide();
-    })
-
-
-    // 페이지 로드 시 실행되는 함수
-    (function checkPopupCookie(){
-
-        // popupClosed라는 이름의 쿠키 값을 가져옴
-        const popupClosed = getCookie("popupClosed");
-
-        // 쿠키 값이 true인 경우에 팝업 숨김
-        if(popupClosed = true) {
-            popUp.hide();
-            overlay.hide();
-        }
-    })();
-
-    // 특정 이름의 쿠키 값을 가져오는 함수
-    function getCookie(name){
-        const cookieArr = document.cookie.split(';');
-        
-        for(let i = 0; i < cookieArr.length; i++){
-            const cookiePair = cookieArr[i].split('=');
-
-            //쿠키 이름과 값의 앞뒤 공백을 제거하는 함수
-            if(name = cookiePair[0].trim()){
-                return decodeURIComponent(cookiePair[i]);
-            }
-        }
-
-    }
-
-    return null
+$(".btn_close").click(function () {
+  $(".main_popup").hide();
+  $(".overlay").hide();
 });
 
+var toggleMainPopup = function () {
+  /* 스토리지 제어 함수 정의 */
+  var handleStorage = {
+    // 스토리지에 데이터 쓰기(이름, 만료일)
+    setStorage: function (name, exp) {
+      // 만료 시간 구하기(exp를 ms단위로 변경)
+      var date = new Date();
+      date = date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+
+      // 로컬 스토리지에 저장하기
+      // (값을 따로 저장하지 않고 만료 시간을 저장)
+      localStorage.setItem(name, date);
+    },
+    // 스토리지 읽어오기
+    getStorage: function (name) {
+      var now = new Date();
+      now = now.setTime(now.getTime());
+      // 현재 시각과 스토리지에 저장된 시각을 각각 비교하여
+      // 시간이 남아 있으면 true, 아니면 false 리턴
+      return parseInt(localStorage.getItem(name)) > now;
+    },
+  };
+
+  // 쿠키 읽고 화면 보이게
+  if (handleStorage.getStorage("today")) {
+    $(".main_popup").removeClass("on");
+    $(".overlay").addClass("on");
+  } else {
+    $(".main_popup").addClass("on");
+    $(".overlay").removeClass("on");
+  }
+
+  // 오늘하루 보지 않기 버튼
+  $(".main_popup").on("click", ".btn_today_close", function () {
+    // 로컬 스토리지에 today라는 이름으로 1일(24시간 뒤) 동안 보이지 않게
+    handleStorage.setStorage("today", 1);
+    $(this).parents(".main_popup.on").removeClass("on");
+    $(".overlay").addClass("on");
+  });
+
+  // 일반 닫기 버튼
+  $(".main_popup").on("click", ".btn_close", function () {
+    $(this).parents(".main_popup.on").removeClass("on");
+  });
+};
+
+$(function () {
+  toggleMainPopup();
+});
